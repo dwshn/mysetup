c70bde1d909bea65eac009f9a6c1ce08897d7cb8
firshandy <firshandydwi@gmail.com>
2023-10-31 15:59:38 +0700

    My Setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        bgcol: '#2D2C2B',
+      },
+      fontFamily: {
+        ubuntu: ['Ubuntu'],
+      },
+    },
+  },
+  plugins: [],
+}
+
